c5a88692760e61f2650fa15cb0ffdd4dd44224d6
joacogonz32 <alumno26.gonzalez.parodi.joaquin@ipm.edu.ar>
2025-05-04 23:14:07 -0300

    Add files via upload

---
+++ Labo_2025-/src/Curso Oracle Java/Customer.java
@@ -0,0 +1,28 @@
+package personas;
+
+public class Customer {
+    private String name;
+
+    public Customer() {
+        this.name = "Joaquin";
+    }
+
+    public Customer(String name){
+            this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+        public static void main(String[] args) {
+           
+            
+
+          
+        }
+    }
