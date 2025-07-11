6c2e5599ae92b2401fdd886aac3f6584083575f9
joacogonz32 <alumno26.gonzalez.parodi.joaquin@ipm.edu.ar>
2025-07-11 12:07:58 -0300

    Add files via upload

---
+++ Tp_Labo/add.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+app.get('/insertar', (req, res) => {
+    let nombre = (req.query.nombre);
+    let apellido = (req.query.apellido);
+    let edad = (req.query.edad);
+    let pais = (req.query.pais);
+    let planElegido = (req.query.planElegido);
+    var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'alumno',
+  password : 'alumnoipm',
+  database : 'PaginaBarcelona'
+});
+ 
+connection.connect();
+ 
+connection.query("insert into Inscripcion values(null,'"+nombre+"','"+apellido+"',"+edad+",'"+pais+"','"+planElegido+"')" , function (error, results, fields) {
+  if (error) throw error;
+  // agregar q vuelva a la pagina inicio
+  res.send("Inscripccion realizada");
+});
+ 
+connection.end();
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
