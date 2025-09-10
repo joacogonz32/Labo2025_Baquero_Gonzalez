fb9ad6c1803423f1804c656de0cb96279ccf5d00
JuanBaqueroPablo <166631434+JuanBaqueroPablo@users.noreply.github.com>
2025-09-10 00:53:43 -0300

    Update GaseosasBeneficio.java

--- Labo_2025-/src/CampeonatoArcoFlecha/GaseosasBeneficio.java
+++ Labo_2025-/src/CampeonatoArcoFlecha/GaseosasBeneficio.java
@@ -4,6 +4,18 @@ import java.util.HashSet;
 public class GaseosasBeneficio implements Acumulable{
     private double precio;
     private HashSet<String> gaseosasConBeneficio;
+     public GaseosaBeneficio(String nombre, String creador, HashSet<String> gaseosas) {
+        super(nombre, creador);
+        this.gaseosas = gaseosas;
+    }
+
+    public HashSet<String> getGaseosasConBeneficio() {
+        return gaseosasGaseosasConBeneficio;
+    }
+
+    public void setGaseosas(HashSet<String> gaseosas) {
+        this.gaseosas = gaseosas;
+    }
 
     @Override
     public double aplicarDescuentoExtra() {
